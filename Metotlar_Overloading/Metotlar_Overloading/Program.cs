3eaa11455d1b7b272fb50d69e5280bd23ab7c3a4
imustafak <89648183+imustafak@users.noreply.github.com>
2022-06-17 16:10:36 +0300

    Add files via upload

---
+++ Metotlar_Overloading/Metotlar_Overloading/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //Metot Aşırı Yükeleme - OverLoading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Mustafa", "Korkmaz");
+            Console.ReadLine();
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a, int b, int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
